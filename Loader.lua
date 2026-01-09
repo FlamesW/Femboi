4133da0eac685a9ad19ce3f171fd444b5796082f
FlamesW <undergodbreaker@gmail.com>
2026-01-09 21:26:25 +0200

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,1 +1,21 @@
+-- // Femboi Loader
+local Scripts = {
+    [1240123653] = "https://raw.githubusercontent.com/FlamesW/Femboi/home/Scripts/ZombieAttack.lua",
+}
 
+local function LoadFile(File)
+    if File then
+        local Success, Err = pcall(function()
+            loadstring(game:HttpGet(File))();
+        end)
+    
+        if not Success then
+            warn("Error at:", File, Err)
+        end
+    end
+end
+
+local __Selected = Scripts[game.PlaceId];
+if __Selected then
+    LoadFile(__Selected) else warn("Unsupported Game");
+end
