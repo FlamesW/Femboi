cc9131cb744fffeadfc6ae7a7b8ba0bf249b0206
FlamesW <undergodbreaker@gmail.com>
2026-01-09 21:35:59 +0200

    Enhance Loader.lua with idle detection and script loading

--- Loader.lua
+++ Loader.lua
@@ -1,4 +1,18 @@
--- // Femboi Loader
+-- // Made by Mommy Flames
+if not game:IsLoaded() then
+    game.Loaded:Wait();
+end
+
+local VirtualUser = game:GetService("VirtualUser");
+local Players = game:GetService("Players");
+local LocalPlayer = Players.LocalPlayer
+
+LocalPlayer.Idled:connect(function()
+    VirtualUser:Button2Down(Vector2.new(0,0), workspace.CurrentCamera.CFrame);
+    task.wait(1);
+    VirtualUser:Button2Up(Vector2.new(0,0), workspace.CurrentCamera.CFrame);
+end)
+
 local Scripts = {
     [1240123653] = "https://raw.githubusercontent.com/FlamesW/Femboi/home/Scripts/ZombieAttack.lua",
 }
