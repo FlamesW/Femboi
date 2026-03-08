50dbb3aad47d2b20be5f01e1a77114f580eca995
Flameh <undergodbreaker@gmail.com>
2026-03-08 16:12:58 +0200

    Refactor Loader.lua for better service management

--- Loader.lua
+++ Loader.lua
@@ -1,35 +1,32 @@
 -- // Made by Mommy Flames
+local Femboi = { GetService = function(service)
+	return cloneref and cloneref(game:GetService(service)) or game:GetService(service)
+end}
+
 if not game:IsLoaded() then
     game.Loaded:Wait();
 end
 
-local VirtualUser = game:GetService("VirtualUser");
-local Players = game:GetService("Players");
-local LocalPlayer = Players.LocalPlayer
+local Players: Players = Femboi.GetService("Players")
+local LocalPlayer = Players.LocalPlayer or Players.PlayerAdded:Wait();
+local InMaintenance = false
 
-LocalPlayer.Idled:connect(function()
-    VirtualUser:Button2Down(Vector2.new(0,0), workspace.CurrentCamera.CFrame);
-    task.wait(1);
-    VirtualUser:Button2Up(Vector2.new(0,0), workspace.CurrentCamera.CFrame);
-end)
+if InMaintenance == true then
+    LocalPlayer:Kick("Femboi is temporarily closed, Try again later~");
+    return
+end
 
-local Scripts = {
-    [1240123653] = "https://raw.githubusercontent.com/FlamesW/Femboi/home/Scripts/ZombieAttack.lua",
+Femboi.Services = {["VirtualUser"] = Femboi.GetService("VirtualUser"),
+	["CoreGui"] = Femboi.GetService("CoreGui"),
+	["Connections"] = getconnections or get_signal_cons
 }
 
-local function LoadFile(File)
-    if File then
-        local Success, Err = pcall(function()
-            loadstring(game:HttpGet(File))();
-        end)
-    
-        if not Success then
-            warn("Error at:", File, Err)
-        end
-    end
-end
+local Repo = "https://raw.githubusercontent.com/FlamesW/Femboi/home/Scripts/"
 
-local __Selected = Scripts[game.PlaceId];
-if __Selected then
-    LoadFile(__Selected) else warn("Unsupported Game");
-end
+local Games = {
+    [3576655] = Repo .. "ZombieAttack.lua",
+    [1098481985] = Repo .. "ReelABrainrot.lua",
+}
+
+if not shared.__ then if Femboi.Services.Connections then for a,b in pairs(Femboi.Services.Connections(LocalPlayer.Idled))do if b.Disable then b:Disable()elseif b.Disconnect then b:Disconnect()end end else Anti_Idle=LocalPlayer.Idled:Connect(function()Femboi.Services.VirtualUser:CaptureController()Femboi.Services.VirtualUser:ClickButton2(Vector2.new())end)end;shared.__=true end
+local a = Games[game.CreatorId]if a then local b,c=pcall(function()loadstring(game:HttpGet(a))("Mommy was here<3~")end)if not b then warn("Failed to load:",a,c)end else warn("💔 Unsupported Game~")end;
